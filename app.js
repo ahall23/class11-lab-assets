5d4ad36eb125acdeb958bd1b6d13d7cd544c16f5
Alexia Hall <alexiahall306@icloud.com>
2023-03-31 19:56:06 -0500

    23

--- app.js
+++ app.js
@@ -8,24 +8,24 @@ function Product(name, imagePath) {
 
 let bag = new Product("bag", "./images/bag.jpg");
 let bananas = new Product("bananas", "./images/banana.jpg");
-let bathroom = new Product("Gbathroom", "./images/bathroom.jpg");
+let bathroom = new Product("bathroom", "./images/bathroom.jpg");
 let boots = new Product("boots", "./images/boots.jpg");
 let breakfast = new Product("breakfast", "./images/breakfastjpg");
 let bubbleGum = new Product("bubbleGum", "./images/bubbleGum.jpg");
 let chair = new Product("chair", "./images/chair.jpg");
-let hulu = new Product("hulu", "./images/hulu.jpg");
+let cthulhu = new Product("cthulhu", "./images/cthulhu.jpg");
 let dog = new Product("dog", "./images/dog.jpg");
 let dragon = new Product("dragon", "./images/dragon.jpg");
 let pen = new Product("pen", "./images/pen.jpg");
-let pet = new Product("pet", "./images/pet.jpg");
+let pet = new Product("pet", "./images/pet-sweep.jpg");
 let scissors = new Product("scissors", "./images/scissors.jpg");
 let shark = new Product("shark", "./images/shark.jpg");
 let sweep = new Product("sweep", "./images/sweep.jpg");
 let tauntaun = new Product("tauntaun", "./images/tauntaun.jpg");
 let unicorn = new Product("unicorn", "./images/unicorn.jpg");
 let water = new Product("water", "./images/water.jpg");
-
-let product = new state.allproductArray.push(
+let wine = new Product("wine", "./images/wine-glass.jpg");
+let Productarray = [
   bag,
   bananas,
   bathroom,
@@ -33,7 +33,7 @@ let product = new state.allproductArray.push(
   breakfast,
   bubbleGum,
   chair,
-  hulu,
+  cthulhu,
   dog,
   dragon,
   pen,
@@ -44,11 +44,44 @@ let product = new state.allproductArray.push(
   tauntaun,
   unicorn,
   water,
-  wine
+  wine,
+];
+
+// let numbers = {
+// one: 1,
+// two: 2,
+// three: 3,
+// Sum: function () {
+//   this.one + this.two + this.three;
+//   return this.one + this.two + this.three;
+// },
+// };
+// numbers.Sum();
+// let firstNumber = numbers.one;
+// let Sum = numbers.Sum();
+// // this is a single operation.
+function getRandomproduct() {
+  // we want to set a randon set randomindex to a random number between 0 and 19
+  let randomIndex = 2;
+  let randomProduct = Productarray[randomIndex];
+  return randomProduct;
+}
+
+function displayimages(image1, image2, image3) {
+  let image1HTML = document.getElementById("image1");
+  image1HTML.setAttribute("src", image1);
+  let image2HTML = document.getElementById("image2");
+  image2HTML.setAttribute("src", image2);
+  let image3HTML = document.getElementById("image3");
+  image3HTML.setAttribute("src", image3);
+}
+let product1 = getRandomproduct();
+console.log(product1);
+displayimages(
+  product1.imagePath,
+  "./images/chair.jpg",
+  "./images/bubbleGum.jpg"
 );
-
-product1.timesShown++;
-
 function selectProduct() {
   let randomIndex = Math.floor(Math.random() * products.length);
   let product = products[randomIndex];
@@ -58,96 +91,3 @@ function selectProduct() {
     selectProduct();
   }
 }
-
-// function renderduck() {
-//   // call the getRandomNumber
-//   let duck1 = getRandomNumber();
-//   let duck2 = getRandomNumber();
-//   let duck3 = getRandomNumber();
-
-//   while (duck1 === duck2 || duck3) {
-//     duck2 = getRandomNumber();
-//   }
-
-// // function handleDuckClick(event) {
-//   if (event.target === duckContainer) {
-//     alert("Please click on an image");
-//   }
-//   clicks++;
-//   let clickDuck = event.target.alt;
-//   for (let i = 0; i < state.allDuckArray.length; i++) {
-//     if (clickDuck === state.allDuckArray[i].name) {
-//       state.allDuckArray[i].clicks++;
-//       break;
-//     }
-//   }
-//   if (click s === maxClicksAllowed) {
-//     duckContainer.removeEventListener("click", handleDuckClick);
-//     // give the button an event lister and styles so the user
-//     // knows its an active button:
-//     resultButton.addEventListener("click", renderResults);
-//     resultButton.className = "clicks-allowed";
-//     duckContainer.className = "no-voting";
-//   } else {
-//     renderGoats();
-//   }
-// }
-
-// function renderResults() {
-//   let ul = document.querySelector("ul");
-//   for (let i = 0; i < state.allDuckArray.length; i++) {
-//     let li = document.createElement("li");
-//     li.textContent = `${state.allGoatsDuck[i].name} had ${state.allDuckArray[i].views} view and was clicked ${state.allDuckArray[i].clicks} times.`;
-//     ul.appendChild(li);
-//   }
-// }
-
-// executable code
-// let bag = new Duck("bag", "./images/bag.jpg");
-// let bananas = new Duck("bananas", "./images/banana.jpg");
-// let bathroom = new Duck("Gbathroom", "./images/bathroom.jpg");
-// let boots = new Duck("boots", "./images/boots.jpg");
-// let breakfast = new Duck("breakfast", "./images/breakfastjpg");
-// let bubbleGum = new Duck("bubbleGum", "./images/bubbleGum.jpg");
-// let chair = new Duck("chair", "./images/chair.jpg");
-// let hulu = new Duck("hulu", "./images/hulu.jpg");
-// let dog = new Duck("dog", "./images/dog.jpg");
-// let dragon = new Duck("dragon", "./images/dragon.jpg");
-// let pen = new Duck("pen", "./images/pen.jpg");
-// let pet = new Duck("pet", "./images/pet.jpg");
-// let scissors = new Duck("scissors", "./images/scissors.jpg");
-// let shark = new Duck("shark", "./images/shark.jpg");
-// let sweep = new Duck("sweep", "./images/sweep.jpg");
-// let tauntaun = new Duck("tauntaun", "./images/tauntaun.jpg");
-// let unicorn = new Duck("unicorn", "./images/unicorn.jpg");
-// let water = new Duck("water", "./images/water.jpg");
-
-// let product = new state.allproductArray.push(
-//   bag,
-//   bananas,
-//   bathroom,
-//   boots,
-//   breakfast,
-//   bubbleGum,
-//   chair,
-//   hulu,
-//   dog,
-//   dragon,
-//   pen,
-//   pet,
-//   scissors,
-//   shark,
-//   sweep,
-//   tauntaun,
-//   unicorn,
-//   water,
-//   wine
-// );
-
-// render();
-
-// Container.addEventListener("click", handledDuckClick);
-
-// function Product(productName, images, timesBeenshown);{
-//     name:
-// }
