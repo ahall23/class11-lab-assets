e2fdd5df2325ec5cc19c82d940a8841bcd20a006
Alexia Hall <alexiahall306@icloud.com>
2023-04-03 05:01:15 -0500

    0

--- app.js
+++ app.js
@@ -51,8 +51,8 @@ function getRandomproduct(Productarray) {
   // we want to set a randon set randomindex to a random number between 0 and 19
   // look at salmon cookies random # funtion.
   let randomProducts = [];
-  let randomIndex1 = Math.random() * Productsarray.length;
-  randomIndex1 = Math.floor(randomIndex1);
+  let randomIndex1 = Math.random() * Productsarray.length; // getting a # between 0 & 19
+  randomIndex1 = Math.floor(randomIndex1); //rounding 👇🏽 the #
   let randomIndex2 = Math.random() * Productsarray.length;
   randomIndex2 = Math.floor(randomIndex2);
   let randomIndex3 = Math.random() * Productsarray.length;
@@ -69,9 +69,9 @@ function getRandomproduct(Productarray) {
     randomIndex1 = Math.floor(randomIndex1);
   }
 
-  let randomProduct1 = Productarray[randomIndex];
-  let randomProduct2 = Productarray[randomIndex];
-  let randomProduct3 = Productarray[randomIndex];
+  let randomProduct1 = Productarray[randomIndex1]; //declaring random product 1, 2, and 3 = to a random product
+  let randomProduct2 = Productarray[randomIndex2];
+  let randomProduct3 = Productarray[randomIndex3];
 
   randomProducts.push(randomProduct1);
   randomProducts.push(randomProduct2);
@@ -79,18 +79,83 @@ function getRandomproduct(Productarray) {
   return randomProducts;
 }
 let UserClicks = 0;
-letdisplay = document.getElementById("Display");
-displayimages.addEventListener("click", ImageEvents);
+let display = document.getElementById("Display");
+displayimage.addEventListener("click", ImageEvents);
 let img1 = document.getElementById("image1");
 
-function displayimages(image1, image2, image3) {
+function displayimage() {
+  let randomProduct = getRandomproducts(Productarray);
+  let firstRandomProduct = randomProduct[0];
+
   let image1HTML = document.getElementById("image1");
-  image1HTML.setAttribute("src", image1);
+  image1HTML.src = firstRandomProduct.imagePath;
+  firstRandomProduct.timeshown = firstRandomProduct.timesShown + 1;
+
+  console.log(UserClicks);
+
+  let SecondRandondomProduct = randomProduct[1];
+
   let image2HTML = document.getElementById("image2");
-  image2HTML.setAttribute("src", image2);
+  image2HTML.src = SecondRandondomProduct.imagePath;
+  SecondRandondomProduct.timesShown = SecondRandomProduct.timesShown + 1;
+
+  let thirdRandondomProduct = randomProduct[2];
+
   let image3HTML = document.getElementById("image3");
-  image3HTML.setAttribute("src", image3);
+  image3HTML.src = SecondRandondomProduct.imagePath;
+  thirdRandondomProduct.timesShown = SecondRandomProduct.timesShown + 1;
 }
+
+function ImageEvents(event) {
+  UserClicks++;
+  console.log(event);
+}
+
+if (event.target.nodeName === "IMG") {
+  let pickedElement = event.target.getAttribute("src");
+  console.log(pickedElement);
+  for (let index = 0; index < products.lenghth; index++) {
+    if (pickedElement === products[index].imagepath) {
+      product[index].timesclicked = product[index].timesclicked + 1;
+      console.log(product[index].timesclicked);
+    }
+  }
+}
+if (UserClicks >= 25) {
+  document.getElementById("Display").removeEventListener("click", ImageEvents);
+  alert("Out of Votes 😵‍💫 ");
+}
+displayimage();
+
+function Showresults() {
+  //create an ul
+  //loop through products
+  let ul = document.createElement("ul");
+  for (let index = 0; index < products.length; index++) {
+    let li = document.createElement("li");
+    li.innerHTML =
+      products[index].productName +
+      " got " +
+      products[index].timesclicked +
+      " votes " +
+      " and was shown " +
+      products[index].timesShown;
+    ul.append(li);
+  }
+  document.body.append(ul);
+}
+console.log(Showresults);
+//calling a function so the images can show
+
+console.log(products);
+// function displayimage(image1, image2, image3) {
+//   let image1HTML = document.getElementById("image1");
+//   image1HTML.setAttribute("src", image1);
+//   let image2HTML = document.getElementById("image2");
+//   image2HTML.setAttribute("src", image2);
+//   let image3HTML = document.getElementById("image3");
+//   image3HTML.setAttribute("src", image3);
+// }
 
 // let product1 = getRandomproduct();
 // console.log(product1);
